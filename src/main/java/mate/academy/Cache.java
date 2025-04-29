569557d974bd05ced6b67b19eb2bc7a0ee6eb50e
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-29 19:32:35 +0300

    implement cache

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,57 @@
 package mate.academy;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Map<K, V> cache = new ConcurrentHashMap<>();
+
     public V get(K key) {
-        return null;
+        lock.readLock().lock();
+        try {
+            return cache.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
+        lock.writeLock().lock();
+        try {
+            cache.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
 
     }
 
     public void remove(K key) {
-
+        lock.writeLock().lock();
+        try {
+            cache.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        lock.writeLock().lock();
+        try {
+            cache.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        lock.readLock().lock();
+        try {
+            return cache.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
